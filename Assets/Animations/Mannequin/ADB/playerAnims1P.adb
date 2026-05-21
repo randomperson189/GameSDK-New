992f6ddce54e02ae97d26be9e324e855da336d2d
RandomPerson189 <32617562+randomperson189@users.noreply.github.com>
2026-05-21 19:40:52 +0800

    Updated

---
+++ Assets/Animations/Mannequin/ADB/playerAnims1P.adb
@@ -0,0 +1,32 @@
+<AnimDB FragDef="Animations/Mannequin/ADB/playerActions.xml" TagDef="Animations/Mannequin/ADB/playerTags.xml">
+ <FragmentList>
+  <FPMovement>
+   <Fragment BlendOutDuration="0.2" Tags="SDKPistol+iron">
+    <AnimLayer>
+     <Blend ExitTime="0" StartTime="0" Duration="0.2"/>
+     <Animation name="stand_tac_run_pistol_iron_add_1p_01" flags="Loop"/>
+    </AnimLayer>
+   </Fragment>
+   <Fragment BlendOutDuration="0.2" Tags="SDKPistol">
+    <AnimLayer>
+     <Blend ExitTime="0" StartTime="0" Duration="0.2"/>
+     <Animation name="stand_tac_run_pistol_shoulder_add_1p_01" flags="Loop"/>
+    </AnimLayer>
+   </Fragment>
+  </FPMovement>
+  <IdlePose>
+   <Fragment BlendOutDuration="0.2" Tags="SDKPistol+iron">
+    <AnimLayer>
+     <Blend ExitTime="0" StartTime="0" Duration="0.40000001"/>
+     <Animation name="stand_tac_idlePose_pistol_iron_1p_01" flags="Loop"/>
+    </AnimLayer>
+   </Fragment>
+   <Fragment BlendOutDuration="0.2" Tags="SDKPistol">
+    <AnimLayer>
+     <Blend ExitTime="0" StartTime="0" Duration="0.40000001"/>
+     <Animation name="stand_tac_idlePose_hammer_shoulder_1p_01" flags="Loop"/>
+    </AnimLayer>
+   </Fragment>
+  </IdlePose>
+ </FragmentList>
+</AnimDB>
---
+++ Assets/Animations/Mannequin/ADB/playerAnims3P.adb
@@ -0,0 +1,12 @@
+<AnimDB FragDef="Animations/Mannequin/ADB/playerActions.xml" TagDef="Animations/Mannequin/ADB/playerTags.xml">
+ <FragmentList>
+  <MotionIdle>
+   <Fragment BlendOutDuration="0.2" Tags="SDKPistol">
+    <AnimLayer>
+     <Blend ExitTime="0" StartTime="0" Duration="0.2"/>
+     <Animation name="stand_tac_idle_pistol_3p_01" flags="Loop"/>
+    </AnimLayer>
+   </Fragment>
+  </MotionIdle>
+ </FragmentList>
+</AnimDB>
